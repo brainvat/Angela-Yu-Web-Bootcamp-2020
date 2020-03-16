fed9c179b8823d31ed10aff923dee317d26e2ab0
Allen Hammock <brainvat@mac.com>
2020-03-15 23:38:38 -0700

    complete coding exercise 8

--- sections/section_09/coding-exercise-8.js
+++ sections/section_09/coding-exercise-8.js
@@ -2,7 +2,18 @@ function fibonacciGenerator (n) {
 //Do NOT change any of the code above 👆
 
     //Write your code here:
+    var fibSequence = [0, 1];
 
+    if (n < 3) {
+      return fibSequence.slice(0, n);
+    }
+
+    for (var i = 3; i <= n; i++) {
+      var t = fibSequence.length;
+      fibSequence.push(fibSequence[t - 1] + fibSequence[t-2]);
+    }
+
+    return fibSequence;
 
 
 
@@ -13,3 +24,8 @@ function fibonacciGenerator (n) {
 
 //Do NOT change any of the code below 👇
 }
+console.log(fibonacciGenerator(5));
+console.log(fibonacciGenerator(1));
+console.log(fibonacciGenerator(2));
+console.log(fibonacciGenerator(10));
+console.log(fibonacciGenerator(15));
