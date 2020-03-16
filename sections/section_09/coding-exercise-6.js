db48d846c3ff50b3acae22efac84b602f2cde81d
Allen Hammock <brainvat@mac.com>
2020-03-15 23:26:31 -0700

    complete coding exercise 6

--- sections/section_09/coding-exercise-6.js
+++ sections/section_09/coding-exercise-6.js
@@ -4,9 +4,27 @@ function isLeap(year) {
 
     //Write your code here.
 
+    var isLeapYear = "Leap year.";
+    var isNotLeapYear = "Not leap year.";
+    var response = isNotLeapYear;
 
+    if (year % 4 === 0) {
+      if (year % 100 === 0) {
+        if (year % 400 === 0) {
+          response = isLeapYear;
+        }
+      }
+    }
+
+    return response;
 
 
 /**************Don't change the code below****************/
 
 }
+
+console.log(isLeap(2000)); // Leap
+console.log(isLeap(2100)); // Not leap
+console.log(isLeap(2400)); // Leap
+console.log(isLeap(1989)); // Not leap
+console.log(isLeap(1948)); // Leap
