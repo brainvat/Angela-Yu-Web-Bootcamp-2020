70795cdb53c365641a4f77ddb40c3b66c606d300
Allen Hammock <brainvat@mac.com>
2020-03-15 23:14:31 -0700

    initialize coding exercise 6

---
+++ sections/section_09/coding-exercise-6.js
@@ -0,0 +1,12 @@
+function isLeap(year) {
+
+/**************Don't change the code above****************/
+
+    //Write your code here.
+
+
+
+
+/**************Don't change the code below****************/
+
+}
