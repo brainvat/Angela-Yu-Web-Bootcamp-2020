8129ed49db3639d2e638f6e0edf76911a93bece8
Allen Hammock <brainvat@mac.com>
2020-03-15 23:14:00 -0700

    complete coding exercise 5

--- sections/section_09/coding-exercise-5.js
+++ sections/section_09/coding-exercise-5.js
@@ -1,5 +1,15 @@
 function bmiCalculator(weight, height) {
-    return weight/(height*height)
+    var bmi = Math.floor(10 * weight/(height*height))/10;
+
+    var interpretation = "Your BMI is " + bmi;
+    if (bmi < 18.5) {
+        interpretation += ", so you are underweight.";
+    } else if (bmi <= 24.9) {
+        interpretation += ", so you have a normal weight.";
+    } else {
+        interpretation += ", so you are overweight.";
+    }
+    return interpretation;
 }
 
-console.log(bmiCalculator(65, 1.8));
+console.log( bmiCalculator(65, 1.8));
