31c71d4f4e7d13152931c1478d9c2f87a172029f
Allen Hammock <brainvat@mac.com>
2020-03-15 23:29:04 -0700

    initialize coding exercise 8

---
+++ sections/section_09/coding-exercise-8.js
@@ -0,0 +1,15 @@
+function fibonacciGenerator (n) {
+//Do NOT change any of the code above 👆
+
+    //Write your code here:
+
+
+
+
+
+
+
+    //Return an array of fibonacci numbers starting from 0.
+
+//Do NOT change any of the code below 👇
+}
