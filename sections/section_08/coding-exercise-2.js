fec37fd6d5dde2a492a1003f0b81607a2437cf72
Allen Hammock <brainvat@mac.com>
2020-03-15 23:44:26 -0700

    commit solution to coding exercise 2

---
+++ sections/section_08/coding-exercise-2.js
@@ -0,0 +1,16 @@
+function test() {
+    var a = "3";
+    var b = "8";
+
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+var c = a;
+    a = b;
+    b = c;
+
+
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
