3d17cab7911971c56ecded426e20ddb5a10de231
Allen Hammock <brainvat@mac.com>
2020-03-18 21:13:19 -0700

    completed superheroes NPM package manager exercise and related video lessons

---
+++ sections/section_18/node-test/index.js
@@ -0,0 +1,9 @@
+
+var superheroes = require("superheroes");
+var hero = superheroes.random();
+
+var supervillains = require("supervillains");
+var villain = supervillains.random();;
+
+console.log("Your hero is " + hero);
+console.log("Your villain is " + villain);
