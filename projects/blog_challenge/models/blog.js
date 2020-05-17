59887139c8c0b8b8d6acb27f8d17575362846418
Allen Hammock <brainvat@mac.com>
2020-05-17 00:37:57 -0700

    complete challenge 8 of blog project

--- projects/blog_challenge/models/blog.js
+++ projects/blog_challenge/models/blog.js
@@ -1,7 +1,25 @@
 //jshint esversion:6
+// https://www.npmjs.com/package/dateformat
+var dateFormat = require('dateformat');
+dateFormat.masks.blogTime = 'dddd, mmmm dS, yyyy, h:MM:ss TT';
 
 var Blog = (function() {
-  console.log('Blog singleton created')
+  this.posts = [];
+
+  this.publish = function(post) {
+    const now = new Date();
+    const nowString = dateFormat(now, "blogTime");
+
+    post.date = nowString;
+    posts.push(post);
+  };
+
+  this.log = function() {
+    console.log(JSON.stringify(this.posts));
+  };
+
+  console.log('Blog singleton created');
+  return this;
 })();
 
 module.exports = Blog;
