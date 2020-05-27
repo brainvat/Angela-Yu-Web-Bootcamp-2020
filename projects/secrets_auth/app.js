805b64a8548766ed3ca1d984cd95023c1c0d2a0a
Allen Hammock <brainvat@mac.com>
2020-05-27 12:07:07 -0700

    complete lesson 404

--- projects/secrets_auth/app.js
+++ projects/secrets_auth/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const lodash = require('lodash');
+const users = require('./models/users.js');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -18,23 +19,51 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.get('/', function(req, resp) {
-  resp.render('home.ejs');
+  resp.render('home');
 });
 
 app.route('/register')
 
 .get(function(req, resp) {
-  resp.render('register.ejs');
+  resp.render('register', { errorMsg: false });
 })
 
 .post(function(req, resp) {
-  resp.redirect('/');
+  users.register(req.body, function(err, results) {
+    if (!err) {
+      resp.render('secrets');
+    } else {
+      resp.render('register', { errorMsg: err });
+    }
+  });
 });
 
 app.route('/login')
 
 .get(function(req, resp) {
-  resp.render('login.ejs');
+  resp.render('login', { errorMsg: false });
+})
+
+.post(function(req, resp) {
+  users.login(req.body, function(err, found) {
+    if (err) {
+      resp.render('login', { errorMsg: err });
+      console.log(`LOGIN ERR:\n${err}`);
+    } else {
+      if (found) {
+        resp.render('secrets');
+      } else {
+        resp.render('login', { errorMsg: 'User not found' });
+        console.log('User not found');
+      }
+    }
+  });
+});
+
+app.route('/logout')
+
+.get(function(req, resp) {
+  resp.redirect('/');
 })
 
 .post(function(req, resp) {
@@ -44,7 +73,7 @@ app.route('/login')
 app.route('/submit')
 
 .get(function(req, resp) {
-  resp.render('submit.ejs');
+  resp.render('submit');
 })
 
 .post(function(req, resp) {
---
+++ projects/secrets_auth/models/users.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv');
+  dotenv.config();
+}
+
+const mongoose = require('mongoose');
+const _ = require('lodash');
+
+// https://www.npmjs.com/package/dateformat
+const dateFormat = require('dateformat');
+dateFormat.masks.createdTime = 'dddd, mmmm dS, yyyy, h:MM:ss TT';
+
+const userSchema = {
+  email: String,
+  created_on: String,
+  password: String,
+  first_name: String,
+  last_name: String
+};
+const User = mongoose.model('User', userSchema);
+
+var mconnect = '';
+const mauth = process.env.MONGO_USER ? `${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@` : '';
+const mhost = process.env.MONGO_HOST || 'localhost';
+const mport = process.env.MONGO_PORT || 27017;
+if (process.env.USE_ATLAS == 'true') {
+  mconnect = `mongodb://${mauth}${mhost}`;
+} else {
+  mconnect = `mongodb://${mauth}${mhost}:${mport}/usersDB`;
+};
+
+mongoose.connect(mconnect, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).catch(err => {
+  console.log(`Mongoose connection error:\n${mconnect}\n${err}`);
+});
+
+var Users = (function() {
+
+  this.register = function(user_info, callback) {
+    const newUser = new User({
+      email: user_info.username,
+      password: user_info.password,
+      created_on: dateFormat(new Date(), "createdTime")
+    });
+    newUser.save(function(err) {
+      callback(err);
+    });
+  };
+
+  this.login = function(user_info, callback) {
+    User.findOne({
+        email: user_info.username
+      },
+      function(err, foundUser) {
+        if (err) {
+          callback(err, false);
+        } else {
+          if (foundUser) {
+            if (foundUser.password === user_info.password) {
+              callback(err, true);
+            } else {
+              callback(err, false);
+            }
+          } else {
+            callback(err, false);
+          }
+        }
+      });
+  };
+
+
+  console.log('Users singleton created');
+  return this;
+})();
+
+module.exports = Users;
