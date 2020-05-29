b890a9116bb168730f3ff2ba0773218904a9ca69
Allen Hammock <brainvat@mac.com>
2020-05-28 23:21:16 -0700

    fix path ending for auth redirect path

--- projects/secrets_auth/app.js
+++ projects/secrets_auth/app.js
@@ -78,7 +78,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: process.env.GOOGLE_CLIENT_HOST + 'auth/google/secrets',
     passReqToCallback: true
     // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
