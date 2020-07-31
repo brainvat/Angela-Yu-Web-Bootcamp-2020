04d1059ffe4b7a69e1686766edf5ac408dac6168
Pratik Kulkarni <45352266+praaatik@users.noreply.github.com>
2020-07-31 21:11:57 +0530

    Update coding-exercise-6
    Gives output for the year 1948

--- sections/section_09/coding-exercise-6.js
+++ sections/section_09/coding-exercise-6.js
@@ -1,30 +1,24 @@
 function isLeap(year) {
-
-/**************Don't change the code above****************/
-
-    //Write your code here.
-
-    var isLeapYear = "Leap year.";
-    var isNotLeapYear = "Not leap year.";
-    var response = isNotLeapYear;
-
-    if (year % 4 === 0) {
-      if (year % 100 === 0) {
-        if (year % 400 === 0) {
-          response = isLeapYear;
+    
+/**************Don't change the code above****************/    
+    
+    //Write your code here.    
+    
+if(year % 4 === 0){
+    if (year % 100 === 0){
+        if(year % 400 === 0){
+            return "Leap year.";
+        }else{
+            return "Not leap year."
         }
-      }
+    }else{
+        return "Leap year."
     }
+}else{
+      return "Not leap year."
+}
+    
 
-    return response;
-
-
-/**************Don't change the code below****************/
+/**************Don't change the code below****************/    
 
 }
-
-console.log(isLeap(2000)); // Leap
-console.log(isLeap(2100)); // Not leap
-console.log(isLeap(2400)); // Leap
-console.log(isLeap(1989)); // Not leap
-console.log(isLeap(1948)); // Leap
